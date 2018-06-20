46bee877ea11e82226930f36488c2b3aff408ae8
bjboss007 <mobolajihabib@gmail.com>
2018-06-20 11:42:48 +0100

    Update settings.py

--- Xposed/settings.py
+++ Xposed/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3!^#sj*6l1!idt99l7)_r6d5)g0!_&juol$*u1z@77^6j6*t_g'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
